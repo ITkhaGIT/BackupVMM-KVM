94b5b4d2c6acfd3c2768a06219c3c608cf92c385
IronWillDevOps <380671617445r@gmail.com>
2023-10-18 15:55:03 +0000

    add log_monitor.sg

---
+++ scripts/log_monitor.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+LOG_FILE=/var/log/autobackup.log
+
+# Функция для отображения сообщения заданного цвета
+print_colored_message() {
+    local type=$1
+    local message=$2
+
+
+    case "$type" in
+        "ERROR")
+            echo -e "[ \e[31m$type\e[0m ] \t - $(date +%T) - $message"  # Красный цвет
+            ;;
+        "OK")
+            echo -e "[  \e[32m$type\e[0m  ] \t - $(date +%T) - $message"  # Зеленый цвет
+            ;;
+        "INFO")
+            echo -e "[ \e[33m$type\e[0m ] \t - $(date +%T) - $message"  # Синий цвет
+            ;;
+        *)
+            echo "$message"  # По умолчанию без изменения цвета
+            ;;
+    esac
+
+save_in_file  "$type" "$notification_text"
+
+}
+save_in_file() {
+    local type=$1
+    local message=$2
+
+    # Сохраняем уведомление в файл
+    echo -e "[ $type ] \t - $(date +%T) - $message" >> "$LOG_FILE"
+
+}
+# Принимаем аргументы: тип уведомления и текст уведомления
+notification_type=$1
+notification_text=$2
+
+while [[ $# -gt 0 ]]; do
+  case $notification_type in
+    -e|--error)
+      print_colored_message "ERROR" "$notification_text"
+	echo "$2"
+      shift 2
+      ;;
+    -s|--success)
+      print_colored_message "OK" "$notification_text"
+      echo "$2"
+      shift 2
+      ;;
+    -i|--info)
+      print_colored_message "INFO" "$notification_text"
+      shift 2
+      ;;
+    *)
+      exit 1
+      ;;
+  esac
+done
+
